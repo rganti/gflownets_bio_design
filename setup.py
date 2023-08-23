7e71e9c7b8827900c916388277cd2075fb1b73f9
rganti <ramanganti@gmail.com>
2023-08-23 15:21:16 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An implementation of GFlowNets for Biological Sequence Design inspired by Bengio paper.',
+    author='Raman Ganti',
+    license='MIT',
+)
